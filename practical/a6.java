b4f1949941079c944ecdbb516ad0c605c34f0c74
Ibad laskar <ibalaskar6@gmail.com>
2023-02-23 22:50:52 +0530

    Update a6.java

--- practical/a6.java
+++ practical/a6.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 public class a6{    
-    public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
- System.out.println("Enter a number");
-  int n=sc.nextInt();
+     
+    public static void main(String args[]){    
+     int flag=0; 
+     Scanner sc =new Scanner(System.in) ;
+      System.out.println("Enter a number");
+     int n=sc.nextInt(); 
 
-      boolean flag = false;
-      for (int i = 2; i <= n / 2; ++i) {
-        
-        // condition for nonprime number
-        if (n % i == 0) {
-          flag = true;
-          break;
-        }
+     if(n<=1){  
+      System.out.println(n+" is not prime number");      
+     }
+     else{  
+      for(int i=2;i<=n/2;i++){      
+       if(n%i==0){      
+        System.out.println(n+" is not prime number");      
+        flag=1;      
+        break;      
+       }      
       }
-  
-      if (flag==false)
-        System.out.println(n + " is a prime number.");
-      else
-        System.out.println(n + " is not a prime number.");
-    }
-  }
+      
+      
+      if(flag==0) 
+       { System.out.println(n+" is prime number"); }  
+     }  
+   }    
+   }  
