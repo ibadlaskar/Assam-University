050d205daae96a0fe9f8f75ae69a0f127535ae85
Ibad laskar <ibalaskar6@gmail.com>
2023-03-12 21:37:18 +0530

    Update a5.java

--- practical/a5.java
+++ practical/a5.java
@@ -8,12 +8,30 @@ public class a5 {
           System.out.println("Binary representation (approach 1): " + binary1);
       
           // Approach 2: using a loop
-         /* String binary2 = "";
-          while (decimal > 0) {
-            binary2 = (decimal % 2) + binary2;
-            decimal = decimal / 2;
-          }
-          System.out.println("Binary representation (approach 2): " + binary2);*/ 
+         /* import java.util.Scanner;
+public class DecimalToBinary {
+ public static void main(String args[])
+ {
+ int dec_num, quot, i=1, j;
+ int bin_num[] = new int[100];
+ Scanner scan = new Scanner(System.in);
+ System.out.print("Input a Decimal Number : ");
+ dec_num = scan.nextInt();
+ quot = dec_num;
+ while(quot != 0)
+ {
+ bin_num[i++] = quot%2;
+ quot = quot/2;
+ }
+ System.out.print("Binary number is: ");
+ for(j=i-1; j>0; j--)
+ {
+ System.out.print(bin_num[j]);
+ }
+ System.out.print("\n");
+ }
+}
+*/ 
         }
       }
     
